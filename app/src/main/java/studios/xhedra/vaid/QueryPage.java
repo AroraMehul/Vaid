39414f5ea6003f4d6dbbc89c623c1c5d12565c7b
humbletechy <iit2017125@iiita.ac.in>
2018-10-19 23:43:00 +0530

    Added Adhaar verification and quesry page

---
+++ app/src/main/java/studios/xhedra/vaid/QueryPage.java
@@ -0,0 +1,13 @@
+package studios.xhedra.vaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QueryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_page);
+    }
+}
