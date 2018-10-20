bf9c193a185de066c7757833007d162cfb137b58
adhruv1008 <35196418+adhruv1008@users.noreply.github.com>
2018-10-20 13:21:00 +0530

    UI improved

--- app/src/main/java/studios/xhedra/vaid/MainActivity.java
+++ app/src/main/java/studios/xhedra/vaid/MainActivity.java
@@ -1,6 +1,9 @@
 package studios.xhedra.vaid;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
